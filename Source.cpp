5858f9a15764189503912bd9b204a2764ff5b391
tranhoainam <72914670+NamNam0812@users.noreply.github.com>
2020-10-15 17:04:24 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,10 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+{
+	int a,b,c;
+	c= a/b;
+	
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
