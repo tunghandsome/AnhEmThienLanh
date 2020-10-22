81602190462f830e6190e8d8f6ea2a7412308112
ngominhquan1 <01262980747quan@gmail.com>
2020-10-22 15:58:45 +0700

    hieu

--- Source.cpp
+++ Source.cpp
@@ -10,11 +10,18 @@ int tong(int a, int b)
 	
 }
 int hieu(int a, int b);
+{
+	int s;
+	cout << "\n Tong la:";
+	cin >> a >> b;
+	return s = a- b;
+}
 int tich(int a, int b);	
 int main()
 {
 	cout << "\n Tong la:";
 	Tong(a,b);
+	hieu(a,b);
 	system("pause");
 	return 0;
 }
